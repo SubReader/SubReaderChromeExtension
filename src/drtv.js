f10ed84d96767ed8246a6fd03f44e3ace4df202b
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2018-07-25 11:15:31 +0200

    Support DRTV

--- src/drtv.js
+++ src/drtv.js
@@ -1,3 +1,13 @@
 const code = require("raw-loader!babel-loader!./interceptors/drtv");
 const script = document.createElement("script");
 script.textContent = code;
+
+window.addEventListener("subtitles", ({ detail: subtitles }) => {
+  chrome.runtime.sendMessage({ action: "subtitles", payload: subtitles });
+});
+
+window.addEventListener("info", ({ detail: info }) => {
+  chrome.runtime.sendMessage({ action: "info", payload: info });
+});
+
+document.body.appendChild(script);
